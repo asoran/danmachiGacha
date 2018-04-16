807342d5ad861b60c64815f12ef7f6244a703550
Xgraill <33863138+Xgraill@users.noreply.github.com>
2018-04-16 22:09:38 +0200

    lot of things

--- img/data.js
+++ img/data.js
@@ -1,12 +1,65 @@
-var Hitachi_Fushi = {nom: "Hitachi_Fushi",
-et: 4,
-img: "./img/hitachi_fushi.png",
+var Hitachi_Fushi = {
+	nom: "[Fushi-kaden]Hitachi Chigusa",
+	et: 4,
+	img: "./img/hitachi_fushi.png"
 }
-var Ouka_Iron = {nom: "Ouka_Iron",
-et: 4,
-img: "./img/ouka_iron.png",
+var Ouka_Iron = {
+	nom: "[Will of Iron]Kashima Ouka",
+	et: 4,
+	img: "./img/ouka_iron.png"
 }
-var Freya_Beauty = {nom: "Freya_Beauty",
-et: 4,
-img: "./img/freya_beauty.png",
+var Freya_Beauty = {
+	nom: "[Beautiful Admirer]Freya",
+	et: 4,
+	img: "./img/freya_beauty.png"
+}
+var Ais_Sparkle={
+    nom: "[Sparkle Princess]Ais Wallenstein",
+    et: 4,
+    img: "./img/ais_sparkle.png"
+}
+var Lefiya_Awake={
+	nom: "[Elven Awakening]Lefiya Viridis",
+    et: 4,
+    img: "./img/lefiya_Awake.png"
+}
+var Raul_Novice={
+	nom: "[High Novice]Raul Nord",
+    et: 3,
+    img: "./img/Raul_High.png"
+}
+var Mord_Dog={
+	nom: "[Dogfight]Mord Latro",
+    et: 3,
+    img: "./img/Mord_Dog.png"
+}
+var Tione_Back={
+	nom: "[Backdraft]Tione Hiryute",
+    et: 3,
+    img: "./img/Tione_Back.png"
+}
+var Kashima_Solid={
+	nom: "[Solid Resolutions]Kashima Ouka",
+    et: 3,
+    img: "./img/Kashima_Solid.png"
+}
+var Lefiya_Thousand={
+	nom: "[Thousand Elf]Lefiya Viridis",
+    et: 3,
+    img: "./img/Lefiya_Thousand.png"
+}
+var Ais_Sword={
+	nom: "[Sword Princess]Ais Wallenstein",
+    et: 3,
+    img: "./img/Aiz_Sword.png"
+}
+var Asfi_Combat={
+	nom: "[Combat Commander]Asfi Al Andromeda",
+    et: 3,
+    img: "./img/Asfi_Combat.png"
+}
+var Welf_Ignis={
+	nom: "[Ignis]Welf Crozzo",
+    et: 3,
+    img: "./img/Welf_Ignis.png"
 }
