9c59b92765f9a813f4d9562e2bb5321d92ed5736
Xgraill <33863138+Xgraill@users.noreply.github.com>
2018-04-16 22:13:15 +0200

    type add

--- img/data.js
+++ img/data.js
@@ -1,65 +1,84 @@
 var Hitachi_Fushi = {
 	nom: "[Fushi-kaden]Hitachi Chigusa",
 	et: 4,
-	img: "./img/hitachi_fushi.png"
+	img: "./img/hitachi_fushi.png",
+	type:"adv"
 }
 var Ouka_Iron = {
 	nom: "[Will of Iron]Kashima Ouka",
 	et: 4,
-	img: "./img/ouka_iron.png"
+	img: "./img/ouka_iron.png",
+	type:"adv"
 }
 var Freya_Beauty = {
 	nom: "[Beautiful Admirer]Freya",
 	et: 4,
-	img: "./img/freya_beauty.png"
+	img: "./img/freya_beauty.png",
+	type:"sup"
 }
 var Ais_Sparkle={
     nom: "[Sparkle Princess]Ais Wallenstein",
     et: 4,
-    img: "./img/ais_sparkle.png"
+    img: "./img/ais_sparkle.png",
+	type:"adv"
 }
 var Lefiya_Awake={
 	nom: "[Elven Awakening]Lefiya Viridis",
     et: 4,
-    img: "./img/lefiya_Awake.png"
+    img: "./img/lefiya_Awake.png",
+	type:"adv"
 }
 var Raul_Novice={
 	nom: "[High Novice]Raul Nord",
     et: 3,
-    img: "./img/Raul_High.png"
+    img: "./img/Raul_High.png",
+	type:"adv"
 }
 var Mord_Dog={
 	nom: "[Dogfight]Mord Latro",
     et: 3,
-    img: "./img/Mord_Dog.png"
+    img: "./img/Mord_Dog.png",
+	type:"adv"
 }
 var Tione_Back={
 	nom: "[Backdraft]Tione Hiryute",
     et: 3,
-    img: "./img/Tione_Back.png"
+    img: "./img/Tione_Back.png",
+	type:"adv"
 }
 var Kashima_Solid={
 	nom: "[Solid Resolutions]Kashima Ouka",
     et: 3,
-    img: "./img/Kashima_Solid.png"
+    img: "./img/Kashima_Solid.png",
+	type:"adv"
 }
 var Lefiya_Thousand={
 	nom: "[Thousand Elf]Lefiya Viridis",
     et: 3,
-    img: "./img/Lefiya_Thousand.png"
+    img: "./img/Lefiya_Thousand.png",
+	type:"adv"
 }
 var Ais_Sword={
 	nom: "[Sword Princess]Ais Wallenstein",
     et: 3,
-    img: "./img/Aiz_Sword.png"
+    img: "./img/Aiz_Sword.png",
+	type:"adv"
 }
 var Asfi_Combat={
 	nom: "[Combat Commander]Asfi Al Andromeda",
     et: 3,
-    img: "./img/Asfi_Combat.png"
+    img: "./img/Asfi_Combat.png",
+	type:"adv"
 }
 var Welf_Ignis={
 	nom: "[Ignis]Welf Crozzo",
     et: 3,
-    img: "./img/Welf_Ignis.png"
+    img: "./img/Welf_Ignis.png",
+	type:"adv"
+}
+var Lili_Sup={
+	nom: "[Supporter]Liliruca Arde",
+    et: 2,
+    img: "./img/lili_sup.png",
+	type:"adv"
 }
