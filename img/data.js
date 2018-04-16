ae8f1fa259404a3836167c8e7c42625d8845cd2e
Xgraill <33863138+Xgraill@users.noreply.github.com>
2018-04-16 21:47:40 +0200

    add 4* in baner

---
+++ img/data.js
@@ -0,0 +1,12 @@
+var Hitachi_Fushi = {nom: "Hitachi_Fushi",
+et: 4,
+img: "./img/hitachi_fushi.png",
+}
+var Ouka_Iron = {nom: "Ouka_Iron",
+et: 4,
+img: "./img/ouka_iron.png",
+}
+var Freya_Beauty = {nom: "Freya_Beauty",
+et: 4,
+img: "./img/freya_beauty.png",
+}
